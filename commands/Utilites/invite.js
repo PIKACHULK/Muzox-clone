d764fcb1049aa09ab226b83b02d5e25fdbd8b720
NeXuS <85962137+PIKACHULK@users.noreply.github.com>
2023-04-12 05:23:00 +0600

    Update invite.js

--- commands/Utilites/invite.js
+++ commands/Utilites/invite.js
@@ -18,7 +18,24 @@ module.exports = {
     .setLabel(`${client.user.username}`)
     .setStyle("Link")
     .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands`)
-   );
+   )
+
+   .addComponents(
+                
+    new ButtonBuilder()
+.setLabel(`Support server`)
+.setStyle("Link")
+.setURL(`https://discord.gg/SHqAhhZK3X`)
+)
+
+.addComponents(
+                
+    new ButtonBuilder()
+.setLabel(`Vote`)
+.setStyle("Link")
+.setURL(`https://discordbotlist.com/bots/wave-8862/upvote`)
+);
+
 
           
            message.channel.send({components: [row]})
